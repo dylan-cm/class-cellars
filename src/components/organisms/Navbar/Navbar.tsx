130949ae36eceab746968815ce165f72acb6914f
Dylan Modell <dylan.modell.promo@gmail.com>
2023-03-31 15:04:03 -0700

    mobile navigation menu improvements

--- src/components/organisms/Navbar/Navbar.tsx
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -5,12 +5,19 @@ import { HashLink, NavHashLink } from "react-router-hash-link";
 import { ReactComponent as Logo } from "../../../assets/Logo - Light.svg";
 import { ReactComponent as AccountIcon } from "../../../assets/accountIcon.svg";
 import { ReactComponent as CartIcon } from "../../../assets/cartIcon.svg";
+import { ReactComponent as AboutIcon } from "../../../assets/aboutIcon.svg";
+import { ReactComponent as LearnIcon } from "../../../assets/learnIcon.svg";
+import { ReactComponent as CellarIcon } from "../../../assets/cellarIcon.svg";
+import { ReactComponent as OpenIcon } from "../../../assets/hamburgerIcon.svg";
+import { ReactComponent as CloseIcon } from "../../../assets/exitIcon.svg";
+
 import "./Navbar.css";
 
 interface NavbarProps {}
 
 const Navbar = ({ ...props }: NavbarProps) => {
   const [navbarBackground, setNavbarBackground] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   const location = useLocation();
   const getHashClass = (label: string) =>
     "NavButton " + (location.hash.slice(1) === label ? "Active" : "");
@@ -19,54 +26,127 @@ const Navbar = ({ ...props }: NavbarProps) => {
       "NavButton " + (location.pathname.slice(1) === label ? "Active" : "")
     );
   };
-  const isSticky = () => (location.pathname === "/" ? "" : " Sticky");
+  const isSticky = () =>
+    location.pathname !== "/" || menuOpen ? " Sticky" : "";
 
   useEffect(() => {
     if (location.pathname !== "/") setNavbarBackground(true);
     else {
       setNavbarBackground(false);
       document.addEventListener("scroll", () => {
-        if (window.scrollY > 50) setNavbarBackground(true);
+        if (window.scrollY > 24) setNavbarBackground(true);
         else setNavbarBackground(false);
       });
     }
   }, [location.pathname]);
 
+  useEffect(() => {
+    document.body.style.overflow = menuOpen ? "hidden" : "auto";
+  }, [menuOpen]);
+
   return (
-    <div
-      className={"Navbar" + isSticky()}
-      style={{ background: navbarBackground ? "white" : "transparent" }}
-    >
-      <HashLink to="/#top" smooth>
-        <Logo className="Logo" />
-      </HashLink>
-      <div className="NavButtons">
-        <NavHashLink to="/#cellar" className={getHashClass("cellar")} smooth>
-          Cellar
-        </NavHashLink>
-        <NavHashLink to="/#learn" className={getHashClass("learn")} smooth>
-          Learn
-        </NavHashLink>
-        <NavHashLink to="/#about" className={getHashClass("about")} smooth>
-          About
-        </NavHashLink>
-        <div className="RightButtons">
-          <NavHashLink to="account" className={getLinkClass("account")}>
-            <AccountIcon />
+    <>
+      <nav
+        className={
+          "Navbar" +
+          isSticky() +
+          (menuOpen ? " MenuNavBackground" : "") +
+          (navbarBackground || menuOpen ? "" : " NoNavBackground")
+        }
+      >
+        <HashLink to="/#top" smooth onClick={() => setMenuOpen(false)}>
+          <Logo className={"Logo" + (menuOpen ? " LightLogo" : "")} />
+        </HashLink>
+        <div className="NavButtons">
+          <NavHashLink to="/#cellar" className={getHashClass("cellar")} smooth>
+            Cellar
           </NavHashLink>
-          <NavHashLink to="cart" className={getLinkClass("cart")}>
-            <CartIcon />
+          <NavHashLink to="/#learn" className={getHashClass("learn")} smooth>
+            Learn
           </NavHashLink>
-          <HashLink to="/#newsletter" smooth>
-            <div className="SignupButton">Sign Up</div>
-          </HashLink>
+          <NavHashLink to="/#about" className={getHashClass("about")} smooth>
+            About
+          </NavHashLink>
+          <div className="RightButtons">
+            <NavHashLink to="account" className={getLinkClass("account")}>
+              <AccountIcon />
+            </NavHashLink>
+            <NavHashLink to="cart" className={getLinkClass("cart")}>
+              <CartIcon />
+            </NavHashLink>
+            <HashLink to="/#newsletter" smooth>
+              <div className="SignupButton">Sign Up</div>
+            </HashLink>
+          </div>
         </div>
+        <HashLink
+          to="/#newsletter"
+          smooth
+          onClick={() => setMenuOpen(false)}
+          className="Mobile"
+        >
+          <div className={"SignupButton" + (menuOpen ? " LightSignup" : "")}>
+            Sign Up
+          </div>
+        </HashLink>
+        <div
+          className={
+            "MenuOpenButton Mobile" + (!menuOpen ? " Visible" : " Invisible")
+          }
+          onClick={() => setMenuOpen(true)}
+        >
+          <OpenIcon />
+        </div>
+        <div
+          className={
+            "MenuCloseButton Mobile" + (menuOpen ? " Visible" : " Invisible")
+          }
+          onClick={() => setMenuOpen(false)}
+        >
+          <CloseIcon />
+        </div>
+      </nav>
+      <div
+        className={"Menu Mobile"}
+        style={{ display: menuOpen ? "flex" : "none" }}
+      >
+        <NavHashLink
+          to="/#cellar"
+          className={"MenuLink " + getHashClass("cellar")}
+          onClick={() => setMenuOpen(false)}
+        >
+          <CellarIcon /> Cellar
+        </NavHashLink>
+        <NavHashLink
+          to="/#learn"
+          className={"MenuLink " + getHashClass("learn")}
+          onClick={() => setMenuOpen(false)}
+        >
+          <LearnIcon /> Learn
+        </NavHashLink>
+        <NavHashLink
+          to="/#about"
+          className={"MenuLink " + getHashClass("about")}
+          onClick={() => setMenuOpen(false)}
+        >
+          <AboutIcon /> About
+        </NavHashLink>
+        <NavHashLink
+          to="account"
+          className={"MenuLink " + getLinkClass("account")}
+          onClick={() => setMenuOpen(false)}
+        >
+          <AccountIcon /> Account
+        </NavHashLink>
+        <NavHashLink
+          to="cart"
+          className={"MenuLink " + getLinkClass("cart")}
+          onClick={() => setMenuOpen(false)}
+        >
+          <CartIcon /> Cart
+        </NavHashLink>
       </div>
-      <HashLink to="/#newsletter" smooth>
-        <div className="SignupButton MobileButton">Sign Up</div>
-      </HashLink>
-      <div className="MenuButton MobileButton" />
-    </div>
+    </>
   );
 };
 
