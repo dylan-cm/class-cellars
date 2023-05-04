d4c193119bfbb75ccd0c5be8e2eb49bc5d5082b8
Dylan Cortez-Modell <dylan.modell@gmail.com>
2023-05-04 13:20:22 -0700

    responsive desktop size

--- src/components/organisms/About/About.tsx
+++ src/components/organisms/About/About.tsx
@@ -6,28 +6,30 @@ interface AboutProps {}
 const About = ({ ...props }: AboutProps) => {
   return (
     <div className="About">
-      <div className="Fun">
-        {/* <img src="someunsplashimgofbarrels" alt=""/> */}
-        <div className="PlaceholderImg">
-          <h1 className="Title">
-            Rare
-            <br />
-            wines,
-          </h1>
-          <h1 className="Title Refined">refined.</h1>
+      <div className="TopRow">
+        <div className="Fun">
+          {/* <img src="someunsplashimgofbarrels" alt=""/> */}
+          <div className="PlaceholderImg">
+            <h1 className="Title">
+              Rare
+              <br />
+              wines,
+            </h1>
+            <h1 className="Title Refined">refined.</h1>
+          </div>
         </div>
+        <p className="Blurb">
+          The mission of Classified Cellars is to offer only the finest bottles
+          of the world’s rarest wines. Before acquiring a bottle for our
+          inventory we ask ourselves if this is a wine we predict will exceed
+          our buyer’s expectations when it is opened for that special occasion.
+          <br />
+          <br />
+          Our commitment to knowledge, experience, and ethics ensures full
+          disclosure of provenance, giving clients confidence in each bottle's
+          authenticity.
+        </p>
       </div>
-      <p className="Blurb">
-        The mission of Classified Cellars is to offer only the finest bottles of
-        the world’s rarest wines. Before acquiring a bottle for our inventory we
-        ask ourselves if this is a wine we predict will exceed our buyer’s
-        expectations when it is opened for that special occasion.
-        <br />
-        <br />
-        Our commitment to knowledge, experience, and ethics ensures full
-        disclosure of provenance, giving clients confidence in each bottle's
-        authenticity.
-      </p>
       <div className="JoinWrapper">
         <h2>
           Come explore our wines so exclusive they should be classified as
--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -8,36 +8,38 @@ interface FooterProps {}
 const Footer = ({ ...props }: FooterProps) => {
   return (
     <div className="Footer">
-      <p>
-        Are you a wine enthusiast with a discerning taste, a vision for an
-        exceptional cellar, and a thirst for new experiences?
-      </p>
-      <h2>Receive insider intel on rare wines.</h2>
-      <EmailCapture />
-      <ul className="Sitemap">
-        <li>
-          <a href="mailto:info@classifiedcellars.com">Contact Us</a>
-        </li>
-        <li>
-          <HashLink to="/#about" smooth>
-            About
-          </HashLink>
-        </li>
-        <li>
-          <HashLink to="/#newsletter" smooth>
-            Home
-          </HashLink>
-        </li>
-      </ul>
-      {/* //TODO: <a href="">Legal Disclaimers</a> */}
-      {/* //TODO:  socials */}
-      <a className="Little" href="legal">
-        Legal Disclaimers
-      </a>
-      <span className="Little">
-        © 2023 Classified Cellars. All Rights Reserved.
-      </span>
-      <span>Made with ❤️ by DMC in sunny Oakland, CA</span>
+      <div className="FooterWrap">
+        <p>
+          Are you a wine enthusiast with a discerning taste, a vision for an
+          exceptional cellar, and a thirst for new experiences?
+        </p>
+        <h2>Receive insider intel on rare wines.</h2>
+        <EmailCapture />
+        <ul className="Sitemap">
+          <li>
+            <a href="mailto:info@classifiedcellars.com">Contact Us</a>
+          </li>
+          <li>
+            <HashLink to="/#about" smooth>
+              About
+            </HashLink>
+          </li>
+          <li>
+            <HashLink to="/#newsletter" smooth>
+              Home
+            </HashLink>
+          </li>
+        </ul>
+        {/* //TODO: <a href="">Legal Disclaimers</a> */}
+        {/* //TODO:  socials */}
+        <a className="Little" href="legal">
+          Legal Disclaimers
+        </a>
+        <span className="Little">
+          © 2023 Classified Cellars. All Rights Reserved.
+        </span>
+        <span>Made with ❤️ by DMC in sunny Oakland, CA</span>
+      </div>
     </div>
   );
 };
