25c8004a067be1dea9f598fad948ebe38e809004
Dylan Cortez-Modell <dylan.modell@gmail.com>
2023-05-04 12:46:31 -0700

    fonts, footer, about

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -1,10 +1,41 @@
 import React from "react";
 import "./Footer.css";
+import EmailCapture from "../../atoms/EmailCapture/EmailCapture";
+import { HashLink } from "react-router-hash-link";
 
 interface FooterProps {}
 
 const Footer = ({ ...props }: FooterProps) => {
-  return <div className="Footer">Footer</div>;
+  return (
+    <div className="Footer">
+      <h2>Receive insider intel on rare wines.</h2>
+      <EmailCapture />
+      <ul className="Sitemap">
+        <li>
+          <a href="mailto:info@classifiedcellars.com">Contact Us</a>
+        </li>
+        <li>
+          <HashLink to="/#about" smooth>
+            About
+          </HashLink>
+        </li>
+        <li>
+          <HashLink to="/#newsletter" smooth>
+            Home
+          </HashLink>
+        </li>
+      </ul>
+      {/* //TODO: <a href="">Legal Disclaimers</a> */}
+      {/* //TODO:  socials */}
+      <a className="Little" href="legal">
+        Legal Disclaimers
+      </a>
+      <span className="Little">
+        © 2023 Classified Cellars. All Rights Reserved.
+      </span>
+      <span>Made with ❤️ by DMC in sunny Oakland, CA</span>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,66 +1,17 @@
 import React from "react";
-// import React, { useEffect, useState } from "react";
-// import News from "../../molecules/News/News";
-// import CellarList from "../../organisms/CellarList/CellarList";
 import Footer from "../../organisms/Footer/Footer";
 import Hero from "../../organisms/Hero/Hero";
 import "./Home.css";
 import { Helmet } from "react-helmet-async";
-
-// type ListedProduct = {
-//   //TODO global product type
-//   id: string;
-//   title: string;
-//   thumbnail: string;
-//   price: number;
-//   year: string;
-//   shortDescription: string;
-// };
+import About from "../../organisms/About/About";
 
 const Home = () => {
-  // const [listedProducts, setListedProducts] = useState<ListedProduct[]>([]);
-
-  //TODO: link setListedProduct to shopify
-  // useEffect(() => {
-  //   setListedProducts([
-  //     {
-  //       id: "0001",
-  //       title: "Dom Perignon",
-  //       thumbnail:
-  //         "https://media.bbr.com/s/bbr/20108000200-ms?img404=Default_Wine",
-  //       price: 355.0,
-  //       year: "2008",
-  //       shortDescription:
-  //         "Now this quiet courtyard, Sunday afternoon, this girl with a random collection of European furniture, as though Deane had once intended to use the place as his home. The tug Marcus Garvey, a steel drum nine meters long and two in diameter, creaked and shuddered as Maelcum punched for a California gambling cartel, then as a gliding cursor struck sparks from the Chinese program’s thrust, a worrying impression of solid fluidity, as though the shards of a broken mirror bent.",
-  //     },
-  //     {
-  //       id: "0001",
-  //       title: "Dom Perignon",
-  //       thumbnail:
-  //         "https://media.bbr.com/s/bbr/20108000200-ms?img404=Default_Wine",
-  //       price: 355.0,
-  //       year: "2008",
-  //       shortDescription:
-  //         "Now this quiet courtyard, Sunday afternoon, this girl with a random collection of European furniture, as though Deane had once intended to use the place as his home. The tug Marcus Garvey, a steel drum nine meters long and two in diameter, creaked and shuddered as Maelcum punched for a California gambling cartel, then as a gliding cursor struck sparks from the Chinese program’s thrust, a worrying impression of solid fluidity, as though the shards of a broken mirror bent.",
-  //     },
-  //     {
-  //       id: "0001",
-  //       title: "Dom Perignon",
-  //       thumbnail:
-  //         "https://media.bbr.com/s/bbr/20108000200-ms?img404=Default_Wine",
-  //       price: 355.0,
-  //       year: "2008",
-  //       shortDescription:
-  //         "Now this quiet courtyard, Sunday afternoon, this girl with a random collection of European furniture, as though Deane had once intended to use the place as his home. The tug Marcus Garvey, a steel drum nine meters long and two in diameter, creaked and shuddered as Maelcum punched for a California gambling cartel, then as a gliding cursor struck sparks from the Chinese program’s thrust, a worrying impression of solid fluidity, as though the shards of a broken mirror bent.",
-  //     },
-  //   ]);
-  // }, []);
-
   return (
     <>
       <Helmet></Helmet>
       <div className="Home">
         <Hero />
+        <About />
         <Footer />
       </div>
     </>
