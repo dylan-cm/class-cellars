baec56ea0b92e258517636b926488e29d3a7bf75
Dylan Cortez-Modell <dylan.modell@gmail.com>
2023-05-08 13:44:15 -0700

    added more visually popping email signup cta

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -3,46 +3,60 @@ import "./Footer.css";
 import EmailCapture from "../../atoms/EmailCapture/EmailCapture";
 import { HashLink } from "react-router-hash-link";
 import Wave from "../../../assets/footer_layered_wave.svg";
+import TopWave from "../../../assets/cta_wave_top.svg";
+import BottomWave from "../../../assets/cta_wave_bottom.svg";
 import Shh from "../../../assets/shh.jpg";
 
 interface FooterProps {}
 
 const Footer = ({ ...props }: FooterProps) => {
   return (
-    <div className="Footer">
-      <img className="Wave" src={Wave} alt="Footer" />
-      <div className="FooterBackgroundColor" />
-      <div className="ShhGradient" />
-      <img className="Shh" src={Shh} alt="Secret" />
-      <div className="FooterWrap">
-        <h2>Join the Classified Cellars Community</h2>
-        <EmailCapture />
-        <ul className="Sitemap">
-          <li>
-            <a href="mailto:info@classifiedcellars.com">Contact Us</a>
-          </li>
-          <li>
-            <HashLink to="/#about" smooth>
-              About
-            </HashLink>
-          </li>
-          <li>
-            <HashLink to="/#newsletter" smooth>
-              Home
-            </HashLink>
-          </li>
-        </ul>
-        {/* //TODO: <a href="">Legal Disclaimers</a> */}
-        {/* //TODO:  socials */}
-        <a className="Little" href="legal">
-          Legal Disclaimers
-        </a>
-        <span className="Little">
-          © 2023 Classified Cellars. All Rights Reserved.
-        </span>
-        <span>Made with ❤️ by DMC in sunny Oakland, CA</span>
+    <>
+      <div className="SignUpCTA">
+        <img className="CTAWave" src={TopWave} alt="Email Signup" />
+        <div className="CTAForm">
+          <h2>We invite you to come explore our fine wines.</h2>
+          <h3>Join the Classified Cellars Community today!</h3>
+          <EmailCapture />
+          <h6>
+            *You can unsubscribe at any time and we will never sell your data.
+          </h6>
+        </div>
+        <img className="CTAWave" src={BottomWave} alt="Email Signup" />
       </div>
-    </div>
+      <div className="Footer">
+        <img className="Wave" src={Wave} alt="Footer" />
+        <div className="FooterBackgroundColor" />
+        <div className="ShhGradient" />
+        <img className="Shh" src={Shh} alt="Secret" />
+        <div className="FooterWrap">
+          <ul className="Sitemap">
+            <li>
+              <a href="mailto:info@classifiedcellars.com">Contact Us</a>
+            </li>
+            <li>
+              <HashLink to="/#about" smooth>
+                About
+              </HashLink>
+            </li>
+            <li>
+              <HashLink to="/#newsletter" smooth>
+                Home
+              </HashLink>
+            </li>
+          </ul>
+          {/* //TODO: <a href="">Legal Disclaimers</a> */}
+          {/* //TODO:  socials */}
+          <a className="Little" href="legal">
+            Legal Disclaimers
+          </a>
+          <span className="Little">
+            © 2023 Classified Cellars. All Rights Reserved.
+          </span>
+          <span>Made with ❤️ by DMC in sunny Oakland, CA</span>
+        </div>
+      </div>
+    </>
   );
 };
 
