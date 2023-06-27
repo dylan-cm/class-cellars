cb3f509bc04e1fb80e000ee3359264f44283fd83
Dylan Cortez-Modell <dylan.modell@gmail.com>
2023-06-26 20:01:10 -0700

    restyled hero to look less like a popup

--- src/components/organisms/Hero/Hero.tsx
+++ src/components/organisms/Hero/Hero.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import "./Hero.css";
-import { ReactComponent as Blobs } from "../../../assets/wine_stains.svg";
 import { useLocation } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 
@@ -24,32 +23,25 @@ const Hero = ({ ...props }: HeroProps) => {
   return (
     <div className="Hero">
       <div
-        className="HeroLeft"
+        className="HeroBG"
         style={{
           backgroundImage: `url(https://images.unsplash.com/photo-1504279577054-acfeccf8fc52?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2274&q=80)`,
         }}
       >
         <div className="GradientLayer" />
       </div>
-      <div className="HeroRight">
-        <div className="Blobs">
-          <Blobs />
-        </div>
-        <div className="HeroRightContent">
-          <h1>
-            Experience a collection of wines curated expressly for your reliance
-            on their premium provenance.
-          </h1>
-          <div className="CaptureWrapper">
-            <h2>
-              We invite you to sign up for our mailing list to assure your
-              receiving our exclusive offers.
-            </h2>
-            <HashLink to="/#newsletter" smooth onClick={focusInput}>
-              <div className="JoinButton">Get Insider Access</div>
-            </HashLink>
-          </div>
-        </div>
+      <div className="HeroContent">
+        <h1>
+          Experience a collection of wines curated expressly for your reliance
+          on their premium provenance.
+        </h1>
+        <h2>
+          We invite you to sign up for our mailing list to assure your receiving
+          our exclusive offers.
+        </h2>
+        <HashLink to="/#newsletter" smooth onClick={focusInput}>
+          <div className="JoinButton">Get Insider Access</div>
+        </HashLink>
       </div>
     </div>
   );
